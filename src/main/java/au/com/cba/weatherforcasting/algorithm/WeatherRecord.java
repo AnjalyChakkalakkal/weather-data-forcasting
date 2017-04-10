269c85c2d62f00ce16ecb6a65f66b50e67e5aa01
Anjaly Chakkalakkal <anjalyc2011@gmail.com>
2017-04-10 21:18:09 +1000

    Weather record represent weather data for a particular day.
    Change-Id: Icbf7c9517e25f683edc022dae2568be94f9cb6c5

---
+++ src/main/java/au/com/cba/weatherforcasting/algorithm/WeatherRecord.java
@@ -0,0 +1,83 @@
+/*****************************************************************************
+ * Copyright (C) 2017-2018 Anjaly Chakkalakkal 
+ * 
+ * All rights reserved.  Licensed to the Apache Software Foundation (ASF) 
+ * under one or more contributor license agreements. See the NOTICE file 
+ * distributed with this work for additional information regarding copyright 
+ * ownership.
+ * 
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ ********************************************************************************/
+package au.com.cba.weatherforcasting.algorithm;
+
+import java.util.Date;
+
+/**
+ * <p>
+ * This class represent weather data for a particular day.
+ * </p>
+ * 
+ * @author Anjaly Chakkalakkal
+ *
+ */
+public class WeatherRecord {
+
+	private String station;
+	private Date localStation;
+	private Condition condition;
+	private float temperature;
+	private float pressure;
+	private float humidity;
+
+	public String getStation() {
+		return station;
+	}
+
+	public void setStation(String station) {
+		this.station = station;
+	}
+
+	public Date getLocalStation() {
+		return localStation;
+	}
+
+	public void setLocalStation(Date localStation) {
+		this.localStation = localStation;
+	}
+
+	public Condition getCondition() {
+		return condition;
+	}
+
+	public void setCondition(Condition condition) {
+		this.condition = condition;
+	}
+
+	public float getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(float temperature) {
+		this.temperature = temperature;
+	}
+
+	public float getPressure() {
+		return pressure;
+	}
+
+	public void setPressure(float pressure) {
+		this.pressure = pressure;
+	}
+
+	public float getHumidity() {
+		return humidity;
+	}
+
+	public void setHumidity(float humidity) {
+		this.humidity = humidity;
+	}
+}
