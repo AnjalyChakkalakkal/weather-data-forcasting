012f3df086661bc9cd24d0b8e7339c0f99f2fcaf
Anjaly Chakkalakkal <anjalyc2011@gmail.com>
2017-04-10 21:17:27 +1000

    A enum to represent different weather condition for a day.
    Change-Id: Icee59e7b3058a32c6170cf93657a0b3b3b553b15

---
+++ src/main/java/au/com/cba/weatherforcasting/algorithm/Condition.java
@@ -0,0 +1,29 @@
+/*****************************************************************************
+ * Copyright (C) 2017-2018 Anjaly Chakkalakkal 
+ * 
+ * All rights reserved.  Licensed to the Apache Software Foundation (ASF) 
+ * under one or more contributor license agreements. See the NOTICE file 
+ * distributed with this work for additional information regarding copyright 
+ * ownership.
+ * 
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ ********************************************************************************/
+package au.com.cba.weatherforcasting.algorithm;
+
+/**
+ * <p>
+ * Represents a weather condition.
+ * </p>
+ * 
+ * @author Anjaly Chakkalakkal
+ *
+ */
+
+public enum Condition {
+
+	RAINY, STORMY, SUNNY, CLOUD, HOT, DRY, WET;
+}
