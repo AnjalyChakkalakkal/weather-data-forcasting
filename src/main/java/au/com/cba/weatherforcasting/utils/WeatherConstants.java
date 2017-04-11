e6e91c27fd5297f599aa878c00badebc92189032
Anjaly Chakkalakkal <anjalyc2011@gmail.com>
2017-04-12 01:10:28 +1000

    A constant has been introduced to reduce the hard coding in the code.
    Change-Id: Ie35844b6b04cc3da25147b6aef240c2bd25291f4

---
+++ src/main/java/au/com/cba/weatherforcasting/utils/WeatherConstants.java
@@ -0,0 +1,32 @@
+/*****************************************************************************
+ * Copyright (C) 2017-2018 Anjaly Chakkalakkal 
+ * 
+ * All rights reserved.  Licensed to the Apache Software Foundation (ASF) 
+ * under one or more contributor license agreements. See the NOTICE file 
+ * distributed with this work for additional information regarding copyright 
+ * ownership.
+ * 
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ ********************************************************************************/
+package au.com.cba.weatherforcasting.utils;
+
+/**
+ * <p>
+ * A class to store constant values.
+ * </p>
+ * 
+ * @author Anjaly Chakkalakkal
+ *
+ */
+
+public class WeatherConstants {
+
+	public static final String DATE_FORMAT = "dd/MM/yyyy";
+	public static final String DATA_SEPARATOR = ",";
+	public static final String METADATA_SEPARATOR = "|";
+	public static final String CONFIG_PATH = "/Configuration.properties";
+}
